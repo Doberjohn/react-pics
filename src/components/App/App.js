86003902b0011a1dcdde071d077580f6ef03d021
Doberjohn <johnfanidis@gmail.com>
2019-05-21 09:57:14 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <SearchBar />
+        </div>
+    )
+}
+
+export default App;
